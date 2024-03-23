e6742463ed632c15df75215ddb490b82c5f51951
Anil Parthasarathi <anilparthasarathi@tamu.edu>
2024-03-23 15:15:39 -0500

    Finished calendar

--- src/my-app/src/Pages/CalendarPage.js
+++ src/my-app/src/Pages/CalendarPage.js
@@ -9,10 +9,23 @@ import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 const localizer = momentLocalizer(moment);
 
+const classes = [{title: "CSCE 402: Law and Policy in Cybersecurity", start: new Date(2024, 2, 18, 10, 0), end: new Date(2024, 2, 18, 12, 0)},
+    {title: "CSCE 402: Law and Policy in Cybersecurity", start: new Date(2024, 2, 20, 10, 0), end: new Date(2024, 2, 20, 12, 0)},
+    {title: "CSCE 402: Law and Policy in Cybersecurity", start: new Date(2024, 2, 22, 10, 0), end: new Date(2024, 2, 22, 12, 0)},
+    {title: "CSCE 483: Senior Capstone Design", start: new Date(2024, 2, 19, 8, 0), end: new Date(2024, 2, 19, 10, 0)},
+    {title: "CSCE 483: Senior Capstone Design", start: new Date(2024, 2, 21, 8, 0), end: new Date(2024, 2, 21, 10, 0)},
+    {title: "CSCE 465: Network Security", start: new Date(2024, 2, 19, 13, 0), end: new Date(2024, 2, 19, 15, 0)},
+    {title: "CSCE 465: Network Security", start: new Date(2024, 2, 21, 13, 0), end: new Date(2024, 2, 21, 15, 0)},
+    {title: "CSCE 420: Artificial Intelligence", start: new Date(2024, 2, 18, 16, 0), end: new Date(2024, 2, 18, 18, 0)},
+    {title: "CSCE 420: Artificial Intelligence", start: new Date(2024, 2, 20, 16, 0), end: new Date(2024, 2, 20, 18, 0)},
+    {title: "CSCE 420: Artificial Intelligence", start: new Date(2024, 2, 22, 16, 0), end: new Date(2024, 2, 22, 18, 0)},
+ ];
+
+
 
 const CalendarPage = () => {
 
-    const [eventsList, setEvent] = useState([{}]);
+    const [eventsList, setEvent] = useState(classes);
 
     const selectSlot = 
         useCallback(
@@ -38,7 +51,7 @@ const CalendarPage = () => {
           titleAccessor={"title"}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500 }}
+          style={{ height: 600 }}
           formats={{ weekdayFormat: "dddd" }}
           className="table-drop-shadow rounded-lg bg-[#F3FAFF] p-3"
         />
